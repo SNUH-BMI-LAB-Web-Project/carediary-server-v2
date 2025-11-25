d87b5a4c2c3cd459d69f0e4ef92e0370af74d97b
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-11-25 20:34:59 +0900

    feat: JpaConfig와 BaseTimeEntity 추가

---
+++ src/main/kotlin/kr/io/snuhbmilab/carediaryserverv2/common/config/JpaConfig.kt
@@ -0,0 +1,8 @@
+package kr.io.snuhbmilab.carediaryserverv2.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class JpaConfig
