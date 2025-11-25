1932087c9dde262f3598c6c38e1d61c6a5c634d0
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-11-25 20:22:24 +0900

    feat: 공통 응답 및 응답 코드 추가

---
+++ src/main/kotlin/kr/io/snuhbmilab/carediaryserverv2/common/ResponseCode.kt
@@ -0,0 +1,27 @@
+package kr.io.snuhbmilab.carediaryserverv2.common
+
+import org.springframework.http.HttpStatus
+
+val SUCCESS_MESSAGE = "요청에 성공했습니다."
+val SUCCESS_CODE = "SUCCESS"
+
+enum class SuccessCode(
+    val httpStatus: HttpStatus,
+) {
+    OK(HttpStatus.OK),
+    CREATED(HttpStatus.CREATED),
+    NO_CONTENT(HttpStatus.NO_CONTENT),
+}
+
+interface ErrorCode {
+    val httpStatus: HttpStatus
+    val message: String
+    val name: String
+}
+
+enum class GlobalErrorCode(
+    override val httpStatus: HttpStatus,
+    override val message: String,
+) : ErrorCode {
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "응답 처리 중, 예외가 발생했습니다."),
+}
---
+++ src/main/kotlin/kr/io/snuhbmilab/carediaryserverv2/common/dto/CommonResponse.kt
@@ -0,0 +1,70 @@
+package kr.io.snuhbmilab.carediaryserverv2.common.dto
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import kr.io.snuhbmilab.carediaryserverv2.common.ErrorCode
+import kr.io.snuhbmilab.carediaryserverv2.common.GlobalErrorCode
+import kr.io.snuhbmilab.carediaryserverv2.common.SUCCESS_CODE
+import kr.io.snuhbmilab.carediaryserverv2.common.SUCCESS_MESSAGE
+import kr.io.snuhbmilab.carediaryserverv2.common.SuccessCode
+import org.springframework.core.MethodParameter
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.server.ServerHttpRequest
+import org.springframework.http.server.ServerHttpResponse
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice
+
+data class CommonResponse<T>(
+    val status: Int,
+    val code: String,
+    val message: String,
+    @JsonInclude(value = JsonInclude.Include.NON_NULL)
+    val data: T? = null
+) {
+    companion object {
+        @JvmStatic
+        fun <T> success(successCode: SuccessCode, data: T? = null): CommonResponse<T> =
+            CommonResponse(successCode.httpStatus.value(), SUCCESS_CODE, SUCCESS_MESSAGE, data)
+
+        @JvmStatic
+        fun <T> ok(data: T? = null): CommonResponse<T> = success(SuccessCode.OK, data)
+
+        @JvmStatic
+        fun error(errorCode: ErrorCode): CommonResponse<Unit> =
+            CommonResponse(errorCode.httpStatus.value(), errorCode.name, errorCode.message)
+
+        @JvmStatic
+        fun error(exception: Exception, httpStatus: HttpStatus): CommonResponse<Unit> =
+            CommonResponse(httpStatus.value(), httpStatus.name, exception.message ?: exception.toString())
+
+        @JvmStatic
+        fun error(exception: Exception): CommonResponse<Unit> {
+            val internalErrorCode = GlobalErrorCode.INTERNAL_SERVER_ERROR
+
+            return CommonResponse(
+                internalErrorCode.httpStatus.value(),
+                internalErrorCode.name,
+                "${exception.javaClass.simpleName}: ${exception.message}"
+            )
+        }
+    }
+}
+
+@RestControllerAdvice
+class CommonResponseBodyAdvice : ResponseBodyAdvice<Any> {
+    override fun supports(returnType: MethodParameter, converterType: Class<out HttpMessageConverter<*>?>): Boolean =
+        true
+
+    override fun beforeBodyWrite(
+        body: Any?, returnType: MethodParameter, selectedContentType: MediaType,
+        selectedConverterType: Class<out HttpMessageConverter<*>?>,
+        request: ServerHttpRequest, response: ServerHttpResponse
+    ): Any? {
+        if (body is CommonResponse<*>) {
+            response.setStatusCode(HttpStatus.valueOf(body.status))
+        }
+
+        return body
+    }
+}
