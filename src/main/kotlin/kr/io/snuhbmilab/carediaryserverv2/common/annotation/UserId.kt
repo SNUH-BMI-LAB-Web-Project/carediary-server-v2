a6d637fc2808b9e20470ed7371e6ee9152bb5422
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-12-11 02:01:51 +0900

    feat: UserId 어노테이션 추가
    - AuthenticationPrincipal 어노테이션 대체용 추가

---
+++ src/main/kotlin/kr/io/snuhbmilab/carediaryserverv2/common/annotation/UserId.kt
@@ -0,0 +1,5 @@
+package kr.io.snuhbmilab.carediaryserverv2.common.annotation
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class UserId()
---
+++ src/main/kotlin/kr/io/snuhbmilab/carediaryserverv2/common/config/WebConfig.kt
@@ -0,0 +1,16 @@
+package kr.io.snuhbmilab.carediaryserverv2.common.config
+
+import kr.io.snuhbmilab.carediaryserverv2.common.resolver.UserIdArgumentResolver
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig(
+    private val userIdArgumentResolver: UserIdArgumentResolver
+) : WebMvcConfigurer {
+
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(userIdArgumentResolver)
+    }
+}
