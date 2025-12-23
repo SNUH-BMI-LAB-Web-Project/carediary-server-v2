03624e9c6c527f8f98889c3a83619a8b4a0ed5dc
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-12-23 14:15:21 +0900

    remove: AuthenticationRequired 어노테이션 제거

--- src/main/kotlin/kr/io/snuhbmilab/carediaryserverv2/common/annotation/AuthenticationRequired.kt
+++
@@ -1,8 +0,0 @@
-package kr.io.snuhbmilab.carediaryserverv2.common.annotation
-
-import org.springframework.security.access.prepost.PreAuthorize
-
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-@PreAuthorize("isAuthenticated()")
-annotation class AuthenticationRequired()
