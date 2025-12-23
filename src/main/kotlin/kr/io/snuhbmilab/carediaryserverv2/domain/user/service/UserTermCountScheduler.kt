352b2088ded410408108baf3273281487a50833b
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-12-23 13:15:43 +0900

    refactor: 회기 수 스케쥴링 에러 처리 개선

--- src/main/kotlin/kr/io/snuhbmilab/carediaryserverv2/domain/user/service/UserTermCountScheduler.kt
+++ src/main/kotlin/kr/io/snuhbmilab/carediaryserverv2/domain/user/service/UserTermCountScheduler.kt
@@ -20,17 +20,25 @@ class UserTermCountScheduler(
         logger.info { "UserTermCountScheduler 시작" }
 
         val today = LocalDate.now()
+        var successCount = 0
+        var failCount = 0
 
         userRepository.findAllByFirstDiaryDateIsNotNullAndTermCountGreaterThan(0).forEach { user ->
-            val firstDiaryDate = user.firstDiaryDate!!
-            val termCount = user.termCount
+            runCatching {
+                val firstDiaryDate = user.firstDiaryDate!!
+                val termCount = user.termCount
 
-            if (firstDiaryDate.plusDays(7L * termCount) == today) {
-                user.termCount++
-                logger.info { "사용자 회기 증가: userId=${user.id!!} termCount=${termCount} -> ${user.termCount}" }
+                if (firstDiaryDate.plusDays(7L * termCount) == today) {
+                    user.termCount++
+                    logger.info { "사용자 회기 증가: userId=${user.id!!} termCount=${termCount} -> ${user.termCount}" }
+                    successCount++
+                }
+            }.onFailure { exception ->
+                logger.error(exception) { "사용자 회기 업데이트 실패: userId=${user.id!!}" }
+                failCount++
             }
         }
 
-        logger.info { "UserTermCountScheduler 종료" }
+        logger.info { "UserTermCountScheduler 종료 [성공: $successCount / 실패: ${failCount}]" }
     }
 }
