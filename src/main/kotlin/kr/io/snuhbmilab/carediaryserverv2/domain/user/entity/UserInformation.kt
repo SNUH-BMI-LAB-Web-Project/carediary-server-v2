1cf8c980eec8efa643da1ee87e4cb50581f436f7
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-12-28 02:11:23 +0900

    feat: 사용자 정보 추가사항 DB에 반영

--- src/main/kotlin/kr/io/snuhbmilab/carediaryserverv2/domain/user/entity/UserInformation.kt
+++ src/main/kotlin/kr/io/snuhbmilab/carediaryserverv2/domain/user/entity/UserInformation.kt
@@ -2,6 +2,8 @@ package kr.io.snuhbmilab.carediaryserverv2.domain.user.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -11,6 +13,7 @@ import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
 import org.hibernate.annotations.OnDelete
 import org.hibernate.annotations.OnDeleteAction
+import java.time.LocalDate
 import java.time.YearMonth
 
 @Entity
@@ -46,4 +49,54 @@ class UserInformation(
 
     @Column(name = "current_residence")
     val currentResidence: String? = null,
-)
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "medical_coverage")
+    val medicalCoverage: MedicalCoverage? = null,
+
+    @Column(name = "special_case_registered", columnDefinition = "TINYINT(1)")
+    val specialCaseRegistered: Boolean? = null,
+
+    @Column(name = "special_case_registered_date")
+    val specialCaseRegisteredDate: LocalDate? = null,
+
+    @Column(name = "disability_registered", columnDefinition = "TINYINT(1)")
+    val disabilityRegistered: Boolean? = null,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "disability_status")
+    val disabilityStatus: DisabilityStatus? = null,
+
+    @Column(name = "disability_type")
+    val disabilityType: String? = null,
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "disability_severity")
+    val disabilitySeverity: DisabilitySeverity? = null,
+
+    @Column(name = "social_welfare_service_labels")
+    val socialWelfareServiceLabels: String? = null
+) {
+    enum class MedicalCoverage {
+        //건강보험
+        HEALTH_INSURANCE,
+        //의료급여 1종
+        MEDICAL_AID_1,
+        //의료급여 2종
+        MEDICAL_AID_2,
+        //차상위 1종
+        NEAR_POOR_1,
+        //차상위 2종
+        NEAR_POOR_2
+    }
+
+    enum class DisabilityStatus {
+        REGISTERED,
+        IN_PROGRESS,
+        NOT_REGISTERED
+    }
+
+    enum class DisabilitySeverity {
+        SEVERE, NOT_SEVERE
+    }
+}
