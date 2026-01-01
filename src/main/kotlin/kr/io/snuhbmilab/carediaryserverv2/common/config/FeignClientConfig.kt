98870f3c76ab74d9d901f61f02d14e2d3cfeea76
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-01-02 03:06:45 +0900

    feat: 타임아웃, 재시도 전략 추가

--- src/main/kotlin/kr/io/snuhbmilab/carediaryserverv2/common/config/FeignClientConfig.kt
+++ src/main/kotlin/kr/io/snuhbmilab/carediaryserverv2/common/config/FeignClientConfig.kt
@@ -1,8 +1,30 @@
 package kr.io.snuhbmilab.carediaryserverv2.common.config
 
+import feign.Logger
+import feign.Request
+import feign.Retryer
+import feign.codec.ErrorDecoder
+import kr.io.snuhbmilab.carediaryserverv2.external.exception.ExternalException
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.util.concurrent.TimeUnit
 
 @Configuration
 @EnableFeignClients(basePackages = ["kr.io.snuhbmilab.carediaryserverv2.external"])
-class FeignClientConfig
+class FeignClientConfig {
+
+    @Bean
+    fun feignRequestOptions(): Request.Options = Request.Options(5, TimeUnit.SECONDS, 60, TimeUnit.SECONDS, true)
+
+    @Bean
+    fun feignRetryer(): Retryer = Retryer.NEVER_RETRY
+
+    @Bean
+    fun feignErrorDecoder(): ErrorDecoder = ErrorDecoder { _, response ->
+        throw ExternalException("외부 API 호출 실패: ${response.status()} - ${response.reason()}")
+    }
+
+    @Bean
+    fun feignLoggerLevel(): Logger.Level = Logger.Level.BASIC
+}
