156bd0ec0ee88606d809658910f4b1005d734cdc
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-01-12 22:30:02 +0900

    chore: 불필요한 import문 제거

--- src/main/kotlin/kr/io/snuhbmilab/carediaryserverv2/domain/diary/repository/DiaryKeywordExtractionRepository.kt
+++ src/main/kotlin/kr/io/snuhbmilab/carediaryserverv2/domain/diary/repository/DiaryKeywordExtractionRepository.kt
@@ -1,6 +1,5 @@
 package kr.io.snuhbmilab.carediaryserverv2.domain.diary.repository
 
-import kr.io.snuhbmilab.carediaryserverv2.domain.diary.entity.Diary
 import kr.io.snuhbmilab.carediaryserverv2.domain.diary.entity.DiaryKeywordExtraction
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
